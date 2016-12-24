60831d917894f62ed686381d9a35ae989864d981
josepainumkal <josepainumkal@users.noreply.github.com>
2016-12-23 18:44:17 -0800

    Update notes.py

--- notes.py
+++ notes.py
@@ -22,3 +22,50 @@ repository:
   taskmanager
 
 
+-------------------------------------------------------------------------
+
+@blueprint.route("/<int:id>/start",methods=['PUT'])
+@jwt_required()
+@modelrun_authorization_required
+def start(id):
+    modelrun = ModelRun.query.get(id)
+    if modelrun:
+      if modelrun.progress_state==PROGRESS_STATES['NOT_STARTED']:
+        schemas = load_schemas()
+        schema = schemas[modelrun.model_name]
+        needed_resources = set(schema['resources']['inputs'].keys())
+        available_resources = set([r.resource_type for r in modelrun.resources])
+        if needed_resources==available_resources:
+          modelrun.progress_state = PROGRESS_STATES['QUEUED']
+          # modelrun = modelrun.update()
+          
+          from redis import Redis
+          redis = Redis(host='workerdb', port=6379, db=0)
+          # # # default_queue_length = queue_length()
+          default_queue_length = int(redis.llen('celery'))
+          maxRentedModelsAllowed = int(redis.get('MaxRentedModelsAllowed'))
+          currentRentedModels = int(redis.get('CurrentRentedModels'))
+
+          if default_queue_length>0 and (currentRentedModels < maxRentedModelsAllowed):
+            task_id = celery.send_task('vwadaptor.run', args=[], kwargs={'modelrun_id':modelrun.id}, queue='rentedQueue')
+            currentRentedModels = currentRentedModels+1
+            p = redis.pipeline()
+            p.set('CurrentRentedModels', currentRentedModels)
+            p.execute()
+          else:
+            task_id = celery.send_task('vwadaptor.run', args=[], kwargs={'modelrun_id':modelrun.id})
+
+
+          # task_id = celery.send_task('vwadaptor.run', args=[], kwargs={'modelrun_id':modelrun.id})
+          modelrun.task_id = str(task_id)
+          modelrun = modelrun.update()
+          return jsonify({'message':'ModelRun submitted in queue','modelrun':modelrun_serializer(modelrun)}), 200
+        else:
+          error = {'message':"ModelRun {0} Doesn't have the necessary resources attached".format(modelrun),'missing':list(needed_resources-available_resources)}
+          return jsonify(error), 400
+      else:
+        error = {'message':PROGRESS_STATES_MSG[modelrun.progress_state].format(modelrun_id=modelrun.id)}
+        return jsonify(error), 400
+    else:
+      err = {"message":"ModelRun {0} Not Found".format(id)}
+      return jsonify(err), 404
