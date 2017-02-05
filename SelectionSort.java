57fd3cb8364e1eb1e4dbc56786f419de0d8f1cff
josepainumkal <josepainumkal@users.noreply.github.com>
2017-02-04 16:36:29 -0800

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -12,20 +12,39 @@ public class SelectionSort {
 		int start = 0, end = n;
 		int min, min_pos;
 		
-		while(start<end){
-			min = a[start];
-			min_pos = start;
+		for(int i=0;i<n;i++){
+			min=a[i];
+			min_pos=i;
 			
-			for(int i=start;i<end;i++){
-				if(a[i]<min){
-					min=a[i];
-					min_pos=i;	
+			for(int j=i;j<n;j++){
+				if(a[j]<min){
+					min=a[j];
+					min_pos=j;	
 				}
 			}
-			swap(a,start,min_pos);
-			start++;
-		}		
+			swap(a,i,min_pos);		
+		}
 	}
+	// Below code also works, for uniformity with bubble and insertion sort, above one is used
+// 	void selSort(int a[]){
+// 		int n = a.length;
+// 		int start = 0, end = n;
+// 		int min, min_pos;
+		
+// 		while(start<end){
+// 			min = a[start];
+// 			min_pos = start;
+			
+// 			for(int i=start;i<end;i++){
+// 				if(a[i]<min){
+// 					min=a[i];
+// 					min_pos=i;	
+// 				}
+// 			}
+// 			swap(a,start,min_pos);
+// 			start++;
+// 		}		
+// 	}
 	
 	public static void main(String args[]){
 		SelectionSort selsort= new SelectionSort();
